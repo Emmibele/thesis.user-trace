50d0a8eb13887811babec826f6706289b2ae1117
Emmibele <120519265+Emmibele@users.noreply.github.com>
2024-12-26 17:24:44 +0100

    a surprise tool, that will help us later

---
+++ frontend_log_injector/css_selector_2.auto.js
@@ -0,0 +1,62 @@
+function generateSelector(context) {
+  let index, pathSelector, localName;
+
+  if (context == "null") throw "not an dom reference";
+  // call getIndex function
+  index = getIndex(context);
+
+  while (context.tagName) {
+      // selector path
+
+      // TODO this variant cannot deal with multiple classes
+
+      const className = context.className
+      const idName = context.id
+
+      pathSelector = context.localName + 
+                      ( className ? `.${className}` : "") + 
+                      ( idName ? `#${idName}` : "") +
+                      (pathSelector ? ">" + pathSelector : "");
+
+      context = context.parentNode;
+  }
+  // selector path for nth of type
+  pathSelector = pathSelector + `:nth-of-type(${index})`;
+  return pathSelector;
+}
+
+// get index for nth of type element
+function getIndex(node) {
+  let i = 1;
+  let tagName = node.tagName;
+
+  while (node.previousSibling) {
+  node = node.previousSibling;
+  if (
+      node.nodeType === 1 &&
+      tagName.toLowerCase() == node.tagName.toLowerCase()
+  ) {
+      i++;
+  }
+  }
+  return i;
+}
+
+// load document
+document.addEventListener("DOMContentLoaded", () => {
+  // click on element to get output
+  document.body.addEventListener("click", (e) => {
+      let selector = document.querySelector(".selector");
+      // selector output
+      let output = generateSelector(e.target);
+
+      selector.innerHTML = `<strong>Selector:</strong> ${output}`;
+
+      // element that you select
+      let element = document.querySelector(output);
+      console.log(output);                
+      console.log("Element:", element);                
+  });
+});
+
+// https://dev.to/aniket_chauhan/generate-a-css-selector-path-of-a-dom-element-4aim
