cf5cfaafbb3206b384e7d00b633e4626671b8d6d
Em Abele <Florian.Abele@proleit.com>
2025-01-06 13:44:35 +0100

    cleanup cors TODOs

--- frontend_log_injector/src/util/backend_service.ts
+++ frontend_log_injector/src/util/backend_service.ts
@@ -2,17 +2,14 @@ import { logServiceConfig } from "../config";
 import { logData } from "../data/LogData";
 
 export async function postLog(logData: logData, serviceConfig: logServiceConfig ) {
-  //TODO: CORS
   const path = serviceConfig.getPostUrl();
   const headers = new Headers();
-  // headers.set('Content-Type', 'application/json');
-  headers.set('Content-Type', 'text/plain'); // todo does this even help
+  headers.set('Content-Type', 'application/json');
   headers.set('Accept', 'application/json');
 
   const request = new Request(path, {
     method: 'POST',
     headers: headers,
-    mode: "cors", // todo figure out if this does anything
     body: JSON.stringify(logData)
   })  
 
